c5fdd70cea40d380fa703d90639bf754fcbc15a2
VadymPoklad <vadimpoklad05@gmail.com>
2024-12-21 17:08:19 +0200

    Deleted wrong configuration

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "rds_sg" {
   ingress {
     cidr_blocks = var.private_subnet_cidrs
     from_port   = 5432  
-    to_port     = 5432s
+    to_port     = 5432
     protocol    = "tcp"
   }
 
